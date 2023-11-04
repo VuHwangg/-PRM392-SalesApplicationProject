5bf3e3aeed39846615e0aa5222772c0d4793570d
NVA <vieetanhnguyeenx@gmail.com>
2023-11-04 09:26:02 +0700

    Create Server by Spring

---
+++ Server/PRM392_FinalProject/src/test/java/com/fpt/PRM392_FinalProject/Prm392FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpt.PRM392_FinalProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prm392FinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
