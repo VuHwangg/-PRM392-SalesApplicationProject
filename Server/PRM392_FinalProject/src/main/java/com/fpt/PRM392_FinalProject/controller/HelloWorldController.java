c9a868fa1f7f8849e0a2beeb383d300f1cbda421
NVA <vieetanhnguyeenx@gmail.com>
2023-11-04 09:40:53 +0700

    Testing and add application properties

---
+++ Server/PRM392_FinalProject/src/main/java/com/fpt/PRM392_FinalProject/controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.fpt.PRM392_FinalProject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/hello")
+public class HelloWorldController {
+
+    @GetMapping
+    public String helloWorld() {
+        return "Hello world";
+    }
+}
