d83a4438b1f0f38d0e561feb4f8bcac0d94df34c
Hoàng Chu Anh Vũ <bonneybonny30082002@gmail.com>
2023-11-06 01:44:52 +0700

    Update Account screen

--- PRM392_FinalProject/app/src/main/java/com/example/prm392_finalproject/Activity/ChangeAddressActivity.java
+++ PRM392_FinalProject/app/src/main/java/com/example/prm392_finalproject/Activity/ChangeAddressActivity.java
@@ -1,6 +1,10 @@
 package com.example.prm392_finalproject.Activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,11 +13,27 @@ import com.example.prm392_finalproject.R;
 
 public class ChangeAddressActivity extends AppCompatActivity {
 
+    private EditText edtName, edtPhoneNum, edtAddress;
+    private TextView btnBack;
+    private Button btnUpdate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_address);
 
+        edtName = findViewById(R.id.edt_name_changeaddress);
+        edtPhoneNum = findViewById(R.id.edt_phone_num_changeaddress);
+        edtAddress = findViewById(R.id.edt_address_changeaddress);
+        btnBack = findViewById(R.id.btn_back);
+        btnUpdate = findViewById(R.id.btn_update);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
 
 
         // Ẩn action bar
