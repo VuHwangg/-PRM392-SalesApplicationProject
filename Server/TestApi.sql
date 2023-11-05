b46c9110beca0e0ac87931c6ea9d96988ff517fe
NVA <vieetanhnguyeenx@gmail.com>
2023-11-05 21:53:22 +0700

    Create TestApi.sql

---
+++ Server/TestApi.sql
@@ -0,0 +1,24 @@
+﻿USE [PRM392_DB]
+GO
+
+INSERT INTO [dbo].[tbl_product]
+           ([category]
+           ,[color]
+           ,[description]
+           ,[discount]
+           ,[image]
+           ,[name]
+           ,[price]
+           ,[supplier])
+     VALUES
+           ('true'
+           ,'Pink'
+           ,'Dù đã được ra mắt từ lâu song điện thoại Apple iPhone 6S Plus 128GB vẫn được nhiều người dùng ưa chuộng nhờ vẻ ngoài quyến rũ, hiệu năng mạnh mẽ và camera chụp ảnh ấn tượng.'
+           ,10
+           ,'https://images.unsplash.com/photo-1527455102718-437c64ea37ad?auto=format&fit=crop&q=80&w=987&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+           ,'IPhone 6'
+           ,4590000
+           ,'Apple Inc')
+GO
+
+
