83db380fa0f22e3a03d14608758234d637999b52
johnherry123 <97157371+johnherry123@users.noreply.github.com>
2023-11-06 02:21:56 +0700

    .

--- PRM392_FinalProject/app/src/main/java/com/example/prm392_finalproject/Activity/ChatActivity.java
+++ PRM392_FinalProject/app/src/main/java/com/example/prm392_finalproject/Activity/ChatActivity.java
@@ -3,16 +3,31 @@ package com.example.prm392_finalproject.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.prm392_finalproject.Adapter.ChatAdapter;
+import com.example.prm392_finalproject.DTOModels.Message_DTO;
 import com.example.prm392_finalproject.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
-public class ChatActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
+public class ChatActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    Button button;
+    EditText editMess;
+    ChatAdapter adapter;
+    List<Message_DTO> list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,8 +63,30 @@ public class ChatActivity extends AppCompatActivity {
                 return true;
             }
         });
+        initview();
 
+    }
 
+    private void initview() {
+        list = new ArrayList<>();
+       // adapter.notifyItemRangeInserted(list.size(),li);
+        recyclerView = findViewById(R.id.recyclerview_chat);
+        editMess = findViewById(R.id.editinputtex);
+        button = findViewById(R.id.chat1);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMessToFire();
+            }
+        });
+    }
+
+    private void sendMessToFire() {
+        String str_mess = editMess.getText().toString().trim();
+//        HashMap<String,>
     }
 
 
