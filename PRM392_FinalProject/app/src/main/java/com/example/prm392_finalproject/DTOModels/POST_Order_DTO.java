b1a5404f9f39512390a449d8a6b2fef3cd3d8867
kryska666 <100037119+kryska666@users.noreply.github.com>
2023-11-07 23:38:41 +0700

    fix

--- PRM392_FinalProject/app/src/main/java/com/example/prm392_finalproject/DTOModels/POST_Order_DTO.java
+++ PRM392_FinalProject/app/src/main/java/com/example/prm392_finalproject/DTOModels/POST_Order_DTO.java
@@ -1,17 +1,19 @@
 package com.example.prm392_finalproject.DTOModels;
 
 import java.lang.reflect.Array;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class POST_Order_DTO {
     private int customerID;
     private String customerPhone;
     private String customerAddress;
-    private String orderDate;
+    private LocalDate orderDate;
     private double totalPrice;
     private ArrayList<POST_Cart_Product_DTO> products;
 
-    public POST_Order_DTO(int customerID, String customerPhone, String customerAddress, String orderDate, double totalPrice, ArrayList<POST_Cart_Product_DTO> products) {
+    public POST_Order_DTO(int customerID, String customerPhone, String customerAddress, LocalDate orderDate, double totalPrice, ArrayList<POST_Cart_Product_DTO> products) {
         this.customerID = customerID;
         this.customerPhone = customerPhone;
         this.customerAddress = customerAddress;
@@ -44,14 +46,6 @@ public class POST_Order_DTO {
         this.customerAddress = customerAddress;
     }
 
-    public String getOrderDate() {
-        return orderDate;
-    }
-
-    public void setOrderDate(String orderDate) {
-        this.orderDate = orderDate;
-    }
-
     public double getTotalPrice() {
         return totalPrice;
     }
@@ -66,5 +60,13 @@ public class POST_Order_DTO {
 
     public void setProducts(ArrayList<POST_Cart_Product_DTO> products) {
         this.products = products;
+    }
+
+    public LocalDate getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDate orderDate) {
+        this.orderDate = orderDate;
     }
 }
