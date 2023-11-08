f57214a55ef9ddc0f782cff1864a6216f419e0de
NxTGiang <kidsno95@gmail.com>
2023-11-09 01:15:34 +0700

    update preference

--- PRM392_FinalProject/app/src/main/java/com/example/prm392_finalproject/Session/UserSharedPreference.java
+++ PRM392_FinalProject/app/src/main/java/com/example/prm392_finalproject/Session/UserSharedPreference.java
@@ -33,4 +33,17 @@ public class UserSharedPreference {
         editor.remove(key);
     }
 
+    public void putBooleanValue(String key, boolean value){
+        SharedPreferences sharedPreferences = context.getSharedPreferences(USER_SHARED_PREFERENCE
+                , Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean(key, value);
+        editor.apply();
+    }
+
+    public boolean getBooleanValue(String key) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(USER_SHARED_PREFERENCE
+                , Context.MODE_PRIVATE);
+        return sharedPreferences.getBoolean(key, true);
+    }
 }
