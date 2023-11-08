5c68d780b1614ce68561273e66619f596125d74d
NVA <vieetanhnguyeenx@gmail.com>
2023-11-08 17:59:04 +0700

    Update Order Service

---
+++ Server/PRM392_FinalProject/src/main/java/com/fpt/PRM392_FinalProject/controller/OrderController.java
@@ -0,0 +1,14 @@
+package com.fpt.PRM392_FinalProject.controller;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@RequestMapping("api/v1/products")
+public class OrderController {
+}
