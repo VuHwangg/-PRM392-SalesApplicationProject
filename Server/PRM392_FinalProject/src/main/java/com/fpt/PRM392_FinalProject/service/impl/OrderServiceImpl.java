e53ade9212ec1f5f1a898dc9d7f5162121e6ea27
NVA <vieetanhnguyeenx@gmail.com>
2023-11-09 13:52:38 +0700

    Update Cart Services

--- Server/PRM392_FinalProject/src/main/java/com/fpt/PRM392_FinalProject/service/impl/OrderServiceImpl.java
+++ Server/PRM392_FinalProject/src/main/java/com/fpt/PRM392_FinalProject/service/impl/OrderServiceImpl.java
@@ -3,12 +3,14 @@ package com.fpt.PRM392_FinalProject.service.impl;
 import com.fpt.PRM392_FinalProject.dto.OrderDTOAddRequest;
 import com.fpt.PRM392_FinalProject.dto.OrderDTOResponse;
 import com.fpt.PRM392_FinalProject.dto.OrderDetailDTOResponse;
+import com.fpt.PRM392_FinalProject.entity.Cart;
 import com.fpt.PRM392_FinalProject.entity.Order;
 import com.fpt.PRM392_FinalProject.entity.OrderDetail;
 import com.fpt.PRM392_FinalProject.entity.Product;
 import com.fpt.PRM392_FinalProject.exception.Exception;
 import com.fpt.PRM392_FinalProject.mapper.OrderDetailMapper;
 import com.fpt.PRM392_FinalProject.mapper.OrderMapper;
+import com.fpt.PRM392_FinalProject.repository.CartRepository;
 import com.fpt.PRM392_FinalProject.repository.OrderDetailRepository;
 import com.fpt.PRM392_FinalProject.repository.OrderRepository;
 import com.fpt.PRM392_FinalProject.repository.ProductRepository;
@@ -29,6 +31,7 @@ public class OrderServiceImpl implements OrderService {
     OrderRepository orderRepository;
     OrderDetailRepository orderDetailRepository;
     ProductRepository productRepository;
+    CartRepository cartRepository;
 
     static final int STATUS_ORDERED = 1;
 
@@ -65,27 +68,33 @@ public class OrderServiceImpl implements OrderService {
         // Create new order
         Order order = OrderMapper.toOrder(orderDTOAddRequest);
         order.setStatus(STATUS_ORDERED);
+        order = orderRepository.save(order);
+
+
         // Create order detail add to new order
         List<OrderDetail> orderDetailList = new ArrayList<>();
-        orderDTOAddRequest.getProducts().forEach((orderDetailDTOAddRequest) ->{
+        Order finalOrder = order;
+        orderDTOAddRequest.getProducts().forEach((orderDetailDTOAddRequest) -> {
 
             //checking product exits
             Product product = productRepository
                     .findById(orderDetailDTOAddRequest.getId())
                     .orElseThrow(() -> Exception.badRequest("Product not exits", "api/v1/order"));
+            //Create and save order detail
+            OrderDetail orderDetail = OrderDetail.builder()
+                    .order(Order.builder()
+                            .id(finalOrder.getId())
+                            .build())
+                    .price(product.getPrice() * orderDetailDTOAddRequest.getQuantity())
+                    .quantity(orderDetailDTOAddRequest.getQuantity())
+                    .product(Product.builder().id(product.getId()).build())
+                    .build();
+            orderDetailRepository.save(orderDetail);
 
-            orderDetailList.add(
-                    OrderDetail.builder()
-                            .price(product.getPrice() * orderDetailDTOAddRequest.getQuantity())
-                            .quantity(orderDetailDTOAddRequest.getQuantity())
-                            .product(Product.builder().id(product.getId()).build())
-                            .build()
-            );
+            // Delete cart exits
+            Cart cart = cartRepository.findByCustomer_IdAndProduct_Id(orderDTOAddRequest.getCustomerID(), product.getId());
+            cartRepository.deleteById(cart.getId());
         });
-
-        order.setOrderDetailList(orderDetailList);
-        // Save to database
-        orderRepository.save(order);
         // Return
         return orderDTOAddRequest;
     }
